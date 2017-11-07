aea6978c69e12fc52c5d40849f0464d404117c8c
Samuel Bautista <tadeolinco@gmail.com>
2017-11-07 22:57:34 +0800

    Initial setup

---
+++ desktop/src/com/badgames/box4dead/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.badgames.box4dead.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badgames.box4dead.Box4Dead;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = 728;
+		new LwjglApplication(new Box4Dead(), config);
+	}
+}
